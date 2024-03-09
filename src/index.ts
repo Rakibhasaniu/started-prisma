91e7d29c97014f145d8b782c79935e71bbbc2800
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-03-09 15:25:42 +0600

    user table created

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const main =async () => {
-    const result = await prisma.post.create({
-        data:{
-            title:"this is title",
-            published:true,
-            author:"Rakib hasan"
-        }
-    })
+    // const result = await prisma.post.create({
+    //     data:{
+    //         title:"this is title",
+    //         published:true,
+    //         author:"Rakib hasan"
+    //     }
+    // })
+    const getAllFromDB = await prisma.post.findMany();
+    console.log(getAllFromDB);
 }
 main();
